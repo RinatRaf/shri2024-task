78bd5fbde568d1c4b3c6602e30276acf600bf3b1
Rafikov Rinat <rinat.rafikov5@yandex.ru>
2024-07-10 02:01:34 +0500

    конфиги vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/shri2024-task/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 });
